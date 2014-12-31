5cba32923dc9284895112ceae73dc15fb0bc2b45
Troy Conquer <troy@gonimbly.com>
2014-12-31 14:59:41 -0500

    initial

---
+++ evernote.js
@@ -0,0 +1,18 @@
+(function(){
+  'use strict';
+  var Evernote = require('evernote').Evernote,
+    filter = new Evernote.NoteFilter(),
+    metaFilter = new Evernote.NotesMetadataResultSpec(),
+    developerToken = process.env.EVERNOTE_DEV_TOKEN || 'BAD_TOKEN',
+    client = new Evernote.Client({token: developerToken}),
+    noteStore = client.getNoteStore();
+
+  noteStore.findNotesMetadata(filter, 0, 100, metaFilter, function(err, notesMeta) {
+    if(err){
+      console.error('err',err);
+      return;
+    }
+    console.log('notesMeta',notesMeta);
+  });
+
+})();
